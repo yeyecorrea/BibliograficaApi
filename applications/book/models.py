57323fc1959ca543d352ba3f5292664f670fc3e9
yeyecorrea <yeyecorrea838@gmail.com>
2023-08-07 19:49:02 -0500

    se crean los modelos para la aplicacion book y los demas archivos de configuracion

--- applications/book/models.py
+++ applications/book/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from applications.editorial.models import Editorial
+from applications.author.models import Author
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length = 25, verbose_name="Nombre")
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE, verbose_name="Editorial")
+    author = models.ManyToManyField(Author, verbose_name="Autor")
+    sipnosis = models.TextField()
+    pub = models.DateField(auto_now=False, auto_now_add=False)
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        managed = True
+        verbose_name = 'Book'
