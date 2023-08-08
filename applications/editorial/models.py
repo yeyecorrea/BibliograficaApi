c1ccdc53bd7a1c61eea99227f602abeebcc88282
yeyecorrea <yeyecorrea838@gmail.com>
2023-08-07 19:49:52 -0500

    se crean los modelos de la aplicacion editorial y los demas archivos de configuracion

--- applications/editorial/models.py
+++ applications/editorial/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Editorial(models.Model):
+    name = models.CharField(max_length = 25, verbose_name="Nombre")
+    address = models.CharField(max_length = 150, verbose_name="Direccion")
+    phone = models.CharField(max_length = 15, verbose_name="Numero de telefono")
+    email = models.EmailField(max_length=50, verbose_name="Correo")
+    web_site = models.CharField(max_length = 150, verbose_name="Sitio Web")
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        managed = True
+        verbose_name = 'Editorial'
