e8364cd2825a03c0b1a24dd1bfe6f20cdd8f3da4
yeyecorrea <yeyecorrea838@gmail.com>
2023-08-07 19:47:31 -0500

    se crean los modelos y la demas configuracion para la aplicacion Author

--- applications/author/apps.py
+++ applications/author/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AuthorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'author'
+    name = 'applications.author'
--- applications/author/models.py
+++ applications/author/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length = 50, verbose_name="Nombre")
+    last_name = models.CharField(max_length = 50, verbose_name="Apellidos")
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, verbose_name="Imagen")
+    biografia = models.TextField(verbose_name="Biografia")
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        managed = True
+        verbose_name = 'Autor'
